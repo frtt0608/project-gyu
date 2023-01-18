a4dd1f1ff0f5753146cc3b4c10202c61b2b6cd25
frtt0608 <frtt0608@gmail.com>
2023-01-18 14:57:14 +0900

    230118 | Spring security + jwt

--- src/main/java/org/gyu/develop/domain/common/jwt/JwtAuthenticationFilter.java
+++ src/main/java/org/gyu/develop/domain/common/jwt/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -12,10 +13,15 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
+    private TokenProvider tokenProvider;
 
+    public JwtAuthenticationFilter(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
+        String token = tokenProvider.resolveToken((HttpServletRequest) request);
+        if(token != null && tokenProvider.validation)
     }
 }
---
+++ src/main/java/org/gyu/develop/domain/common/jwt/TokenProvider.java
@@ -0,0 +1,35 @@
+package org.gyu.develop.domain.common.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class TokenProvider {
+
+    @Value("jwt.secretKey")
+    private String secretKey;
+
+    public TokenProvider(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String resolveToken(HttpServletRequest req) {
+        return req.getHeader("X-AUTH-TOKEN");
+    }
+
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
