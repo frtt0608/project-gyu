b54ee7dd847f91171839ffa7be94d063a35d8797
frtt0608 <frtt0608@gmail.com>
2023-01-26 23:33:31 +0900

    230126 | Junit5 | 테스트코드 작성 | gradle 의존성에 따라 라이브러리 활용이 다름

--- src/main/java/org/gyu/develop/domain/common/dto/RequestLogin.java
+++ src/main/java/org/gyu/develop/domain/common/dto/RequestLogin.java
@@ -1,8 +1,10 @@
 package org.gyu.develop.domain.common.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RequestLogin {
     private String email;
     private String password;
---
+++ src/test/java/org/gyu/develop/domain/common/controller/LoginControllerTest.java
@@ -0,0 +1,18 @@
+package org.gyu.develop.domain.common.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoginControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Test
+    void login_테스트() {
+
+    }
+}
