91939decbb442060e440e631a4ed5d7b406c12c4
frtt0608 <frtt0608@gmail.com>
2023-01-28 17:35:05 +0900

    230128 | Junit5 | 로그인 테스트 코드

--- src/main/java/org/gyu/develop/domain/common/controller/LoginController.java
+++ src/main/java/org/gyu/develop/domain/common/controller/LoginController.java
@@ -1,22 +1,25 @@
 package org.gyu.develop.domain.common.controller;
 
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.gyu.develop.domain.common.application.LoginService;
 import org.gyu.develop.domain.common.dto.RequestLogin;
 import org.gyu.develop.domain.common.dto.TokenInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RequiredArgsConstructor
+@NoArgsConstructor
 @RestController
 @RequestMapping("/member")
 public class LoginController {
 
-    private final LoginService loginService;
+    @Autowired
+    private LoginService loginService;
 
     @PostMapping("/login")
     public TokenInfo login(@RequestBody RequestLogin requestLogin) {
--- src/test/java/org/gyu/develop/domain/common/controller/LoginControllerTest.java
+++ src/test/java/org/gyu/develop/domain/common/controller/LoginControllerTest.java
@@ -1,18 +1,54 @@
 package org.gyu.develop.domain.common.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.gyu.develop.domain.common.dto.RequestLogin;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class LoginControllerTest {
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private RequestLogin requestLogin;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private MockMvc mockMvc;
 
-    @Test
-    void login_테스트() {
+    @BeforeEach
+    public void setMockMvc() {
 
+        String email = "frtt0608@naver.com";
+        String password = "new1234!";
+        requestLogin = new RequestLogin(email, password);
+
+        //        this.mockMvc = MockMvcBuilders.standaloneSetup(new LoginController()).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    void login_테스트() throws Exception {
+
+        mockMvc.perform(post("/member/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestLogin)))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 }
